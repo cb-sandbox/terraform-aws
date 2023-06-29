a983b38d7f93b7f829159d2484e6d20ae9c0a63e
naqsh01 <shnaqvi@gmail.com>
2023-06-29 16:29:58 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,23 @@
 terraform {
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "3.5.0"
-    }
-  }
-}
-
-provider "google" {
-
-  project = "flow-workshop"
-  region  = "us-east1"
-  zone    = "us-east1-b"
-}
-
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
   }
 
-  network_interface {
-    network = "default"
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Example EC2"
   }
 }
